c00dbe2d525c932b9ad01b066aa3cc6178ea549e
TristanDubart <tristan.dubart@proton.me>
2024-03-13 09:46:03 +0100

    Exercice1-2 13/03/2024

---
+++ Exercice1-2.ps1
@@ -0,0 +1,10 @@
+﻿
+#NAME : Exercice1-2.ps1
+#AUTHOR : Dubart Tristan, CUB
+#DATE : 13/03/2024
+#
+#VERSION : 1.1
+#COMMENTS : Commande afficher l'ensemble des processus en cours sur le serveur
+#
+
+Get-Process | Where-Object { $_.ProcessName -eq "svchost" } | Format-Table -AutoSize
